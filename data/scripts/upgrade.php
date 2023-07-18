01a57a630e8fea234dc547ce9fd285b86ce26a69
samszo <samszon@gmail.com>
2023-07-18 10:12:37 +0200

    HDR 0.0.2

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -11,6 +11,20 @@ namespace JDC;
  * @var \Doctrine\ORM\EntityManager $entityManager
  * @var \Omeka\Api\Manager $api
  */
+
+ if (!class_exists(\Generic\InstallResources::class)) {
+    if (file_exists(dirname(__DIR__, 3) . '/Generic/InstallResources.php')) {
+        require_once dirname(__DIR__, 3) . '/Generic/InstallResources.php';
+    } elseif (file_exists(__DIR__ . '/InstallResources.php')) {
+        require_once __DIR__ . '/InstallResources.php';
+    } else {
+        // Nothing to install.
+        return $this;
+    }
+}
+$installResources = new \Generic\InstallResources($serviceLocator);
+
+
 $services = $serviceLocator;
 $settings = $services->get('Omeka\Settings');
 $config = require dirname(dirname(__DIR__)) . '/config/module.config.php';
@@ -19,3 +33,14 @@ $entityManager = $services->get('Omeka\EntityManager');
 $plugins = $services->get('ControllerPluginManager');
 $api = $plugins->get('api');
 $space = strtolower(__NAMESPACE__);
+
+
+if (version_compare($oldVersion, '3.0.1', '<')) {
+    $this->checkDependency();
+    $installResources = $installResources();
+    $installResources->checkAllResources('JDC');
+    $installResources->createAllResources('JDC');
+
+}
+
+
